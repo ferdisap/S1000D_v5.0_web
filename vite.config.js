d46f0b99bcc6c44b38d36c03be349da637aa8892
Ferdi Arrahman <ferdisaptoko@gmail.com>
2025-07-06 16:05:37 +0700

    Initial dist subtree commit

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     // root: './',
-    // publicDir: 'src',
+    publicDir: 'src',
     // plugins:[
     //     xml(),
     // ],
